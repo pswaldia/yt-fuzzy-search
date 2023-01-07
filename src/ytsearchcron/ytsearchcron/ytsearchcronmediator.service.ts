f3d619cd82d1f35ff5944fbf73e17cdf91e2bba7
pswaldia1 <pswaldia1@gmail.com>
2023-01-08 03:59:19 +0530

    chore(): added nodejs client library for using google APIs

--- src/ytsearchcron/ytsearchcron/ytsearchcronmediator.service.ts
+++ src/ytsearchcron/ytsearchcron/ytsearchcronmediator.service.ts
@@ -2,16 +2,19 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { VideoEntity } from '../model/video.entity';
+import { YtSearchCronService } from './ytsearchcron.service';
 
 @Injectable()
 export class YtSearchCronMediatorService {
     private readonly logger = new Logger(YtSearchCronMediatorService.name);
+
     constructor(
         @InjectRepository(VideoEntity)
         private userRepository: Repository<VideoEntity>
     ) {}
     
     searchAndPersistVideos(){
-        
+        const methodName: string = "#searchAndPersistVideos"
+        this.logger.log("Request received to call youtubedatav3Search API to search and save youtube video data in persistence storage", [methodName]);
     }
 }
