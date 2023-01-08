b0451a686d5d4d3117a512757077ca77a5cbd033
pswaldia1 <pswaldia1@gmail.com>
2023-01-09 03:08:48 +0530

    chore(): final commit to make cron run every 1 minute

--- src/ytsearchcron/service/ytsearchcron.service.ts
+++ src/ytsearchcron/service/ytsearchcron.service.ts
@@ -8,7 +8,7 @@ export class YtSearchCronService {
 
     constructor(private ytSearchCronMediator: YtSearchCronMediatorService){}
 
-    @Cron('*/5 * * * *')
+    @Cron('* * * * *')
     searchVideos(){
         const methodName: string = "#searchVideos"
         this.logger.log(`${methodName}: Request received to run the job to search for youtube videos for predefined search query.`);
